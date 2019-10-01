8cee39a1f8f3ca5e8f98a1353109aa1b35864a98
Biplap Bhattarai <bhattaraib58@gmail.com>
2019-10-01 21:50:55 +0545

    implemented file reading from google drive, added cliend keys, google-sheet

--- src/index.js
+++ src/index.js
@@ -2,11 +2,14 @@ import "./env";
 import cors from "cors";
 import express from "express";
 import bodyParser from "body-parser";
+import GoogleSpreadsheet from "google-spreadsheet";
 
 const app = express();
 
 const APP_PORT = process.env.APP_PORT || 4000;
 const APP_HOST = process.env.APP_HOST || "127.0.0.1";
+
+const credentials = require("../client_secret.json");
 
 app.use(cors());
 app.use(bodyParser.json());
@@ -16,10 +19,31 @@ app.use(
   })
 );
 
-app.use('/api',(req,res)=>{
+const link =
+  "https://docs.google.com/spreadsheets/d/1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms/edit";
+
+// Create a document object using the ID of the spreadsheet - obtained from its URL.
+const doc = new GoogleSpreadsheet(
+  "1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms"
+);
+
+let resData = null;
+
+doc.useServiceAccountAuth(credentials, err => {
+  console.log(err);
+
+  doc.getRows(1, (error, rows) => {
+    resData = rows;
+    console.log(error);
+    // console.log(rows);
+  });
+});
+
+app.use("/api", (req, res) => {
   res.json({
-    response:{
-      message:'hello api'
+    response: {
+      message: "hello api",
+      data: resData
     }
   });
 });
