eea67221c472db286c2f670ec05576c808bfa662
Biplap Bhattarai <bhattaraib58@gmail.com>
2019-10-02 14:45:36 +0545

    changed json file format

--- src/API/googleDriveExcelAPI.js
+++ src/API/googleDriveExcelAPI.js
@@ -1,22 +1,21 @@
 import GoogleSpreadsheet from "google-spreadsheet";
 
 let credentials = {
-  'type': process.env.type,
-  'project_id': process.env.project_id,
-  'private_key_id': process.env.private_key_id,
-  'private_key': process.env.private_key,
-  'client_email': process.env.client_email,
-  'client_id': process.env.client_id,
-  'auth_uri': process.env.auth_uri,
-  'token_uri': process.env.token_uri,
-  'auth_provider_x509_cert_url': process.env.auth_provider_x509_cert_url,
-  'client_x509_cert_url': process.env.client_x509_cert_url
+  "type": process.env.type,
+  "project_id": process.env.project_id,
+  "private_key_id": process.env.private_key_id,
+  "private_key": process.env.private_key,
+  "client_email": process.env.client_email,
+  "client_id": process.env.client_id,
+  "auth_uri": process.env.auth_uri,
+  "token_uri": process.env.token_uri,
+  "auth_provider_x509_cert_url": process.env.auth_provider_x509_cert_url,
+  "client_x509_cert_url": process.env.client_x509_cert_url
 };
 
 console.log(credentials)
 
-const link =
-  "https://docs.google.com/spreadsheets/d/1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms/edit";
+const link ="https://docs.google.com/spreadsheets/d/1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms/edit";
 
 //its file id is 1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms
 
