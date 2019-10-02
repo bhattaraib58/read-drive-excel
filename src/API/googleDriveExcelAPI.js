1f5783c06f51ab1df8a5094310d6371b2d765496
Biplap Bhattarai <bhattaraib58@gmail.com>
2019-10-02 13:54:20 +0545

    changed client secret to env file

--- src/API/googleDriveExcelAPI.js
+++ src/API/googleDriveExcelAPI.js
@@ -1,8 +1,17 @@
 import GoogleSpreadsheet from "google-spreadsheet";
 
-let credentials =process.env.client_secret || require("../../client_secret.json");
-
-credentials =JSON.parse(JSON.stringify(credentials));
+let credentials = {
+  'type': process.env.type,
+  'project_id': process.env.project_id,
+  'private_key_id': process.env.private_key_id,
+  'private_key': process.env.private_key,
+  'client_email': process.env.client_email,
+  'client_id': process.env.client_id,
+  'auth_uri': process.env.auth_uri,
+  'token_uri': process.env.token_uri,
+  'auth_provider_x509_cert_url': process.env.auth_provider_x509_cert_url,
+  'client_x509_cert_url': process.env.client_x509_cert_url
+};
 
 const link =
   "https://docs.google.com/spreadsheets/d/1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms/edit";
