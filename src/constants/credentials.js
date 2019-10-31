2227b437e46283b8cbcac3b57788aca8439bf560
Biplap Bhattarai <bhattaraib58@gmail.com>
2019-10-31 16:26:47 +0545

    add-worksheet-support: add support for multiple worksheets, private key now be read from env, example env added

---
+++ src/constants/credentials.js
@@ -0,0 +1,16 @@
+import atob from 'atob';
+
+const credentials = {
+  type: process.env.type,
+  project_id: process.env.project_id,
+  private_key_id: process.env.private_key_id,
+  private_key: atob(process.env.private_key), // put btoa encoded key on env
+  client_email: process.env.client_email,
+  client_id: process.env.client_id,
+  auth_uri: process.env.auth_uri,
+  token_uri: process.env.token_uri,
+  auth_provider_x509_cert_url: process.env.auth_provider_x509_cert_url,
+  client_x509_cert_url: process.env.client_x509_cert_url
+};
+
+export default credentials;
